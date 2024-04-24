e887588ded301b4e038ce509d6d1a86fd8f5e734
Samuel Raducan <samuel.thembp@gmail.com>
2024-04-24 16:51:11 +0100

    Created MaskedWord class to render the character or _ for a given word

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         WordChooser wordChooser = new WordChooser();
-        System.out.println(wordChooser.getRandomWord());
+        MaskedWord masked = new MaskedWord();
+        ArrayList<Character> characters = new ArrayList<>(Arrays.asList('S', 'E', 'N', 'E', 'O'));
+
+        String word = wordChooser.getRandomWord();
+        System.out.println(masked.getMaskedWord(word, characters));
     }
 }
---
+++ src/main/java/org/example/MaskedWord.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class MaskedWord {
+    public String getMaskedWord(String word, ArrayList<Character> guessedLetters) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+           Character currentLetter = word.charAt(i);
+            System.out.println(word.charAt(i));
+           if (i == 0) {
+               builder.append(currentLetter);
+           } else {
+               builder.append(guessedLetters.contains(currentLetter) ? currentLetter : '_');
+           }
+        }
+
+        return builder.toString();
+    }
+}
