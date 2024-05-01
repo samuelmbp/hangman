cef47f11c85f34ade641bf508e6f79c5c02d40d7
Samuel Raducan <samuel.thembp@gmail.com>
2024-05-01 09:08:20 +0100

    Added congratulations and sad face ascii

--- src/main/java/org/example/Print.java
+++ src/main/java/org/example/Print.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Print {
 
+    private final String congratulationsAscii = "                                 _       \n" +
+            "                                | |      \n" +
+            "  ___ ___  _ __   __ _ _ __ __ _| |_ ___ \n" +
+            " / __/ _ \\| '_ \\ / _` | '__/ _` | __/ __|\n" +
+            "| (_| (_) | | | | (_| | | | (_| | |_\\__ \\\n" +
+            " \\___\\___/|_| |_|\\__, |_|  \\__,_|\\__|___/\n" +
+            "                  __/ |                  \n" +
+            "                 |___/                ";
+
+
+    private final String sadFace = "(•́︵•̀)";
+
     public void welcomeMessage(String name) {
         System.out.println("Welcome to the Hangman game " + name + "!");
         System.out.println();
@@ -18,6 +30,7 @@ public class Print {
 
     public void congratulateWinner(String name, String word) {
         System.out.println("================================");
+        System.out.println(congratulationsAscii);
         System.out.printf("Congratulations %s! You won! \nThe word was: [%s]", name, word);
         System.out.println();
     }
@@ -31,7 +44,9 @@ public class Print {
     }
 
     public void gameLostMessage(String word) {
-        System.out.printf("Sorry, you lost! The word was: [%s]. Better luck next time!", word);
+        System.out.printf("%s Sorry, you lost! The word was: [%s].", sadFace, word);
+        System.out.println();
+        System.out.println("Better luck next time!");
         System.out.println();
     }
 }
